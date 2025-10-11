32c503321ac360b999684bb6e6874df72638e33d
thomas-wondim <162230710+thomas-wondim@users.noreply.github.com>
2025-10-11 02:27:56 -0400

    Create card.tsx

---
+++ components/components/ui/components/ui/card.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { cn } from "@/components/utils";
+
+
+export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+return <div className={cn("rounded-2xl border bg-white shadow-sm", className)} {...props} />;
+}
+export function CardHeader({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+return <div className={cn("p-6 pb-3", className)} {...props} />;
+}
+export function CardTitle({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
+return <h3 className={cn("text-lg font-semibold", className)} {...props} />;
+}
+export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+return <div className={cn("p-6 pt-0", className)} {...props} />;
+}
